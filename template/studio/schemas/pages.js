1159f30adc8d1e4c8a8e70d0febc3882aecbef21
Bryan Robinson <bryanlrobinson@gmail.com>
2020-09-17 13:56:20 -0400

    Initial commit

---
+++ template/studio/schemas/pages.js
@@ -0,0 +1,28 @@
+export default {
+    name: 'page',
+    title: 'Page',
+    type: 'document',
+        fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+        },
+        {
+            title: 'Slug',
+            name: 'slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 200, // // will be ignored if slugify is set
+                slugify: input => input
+                            .toLowerCase()
+                            .replace(/\s+/g, '-')
+                            .slice(0, 200),
+                isUnique: proposedSlug => true,
+            },
+        },
+        
+        
+    ]
+}
